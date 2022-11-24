07ed18e7309f34446f77336e3bde17822c29676f
Dora Noda <dev@stargately.com>
2022-11-23 20:13:51 -0800

    add antd and styletron

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Provider as StyletronProvider } from "styletron-react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { styletron } from "../src/styletron";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyletronProvider value={styletron}>
+      <Component {...pageProps} />
+    </StyletronProvider>
+  );
 }
